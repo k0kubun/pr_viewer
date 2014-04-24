636203c076874752021bafdfaaf162b8c3555d9b
Takashi Kokubun <takashikkbn@gmail.com>
2014-04-24 23:03:33 +0900

    Create prototype

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,11 +1,46 @@
 package controllers
 
-import "github.com/revel/revel"
+import (
+	"code.google.com/p/goauth2/oauth"
+	"fmt"
+	"github.com/google/go-github/github"
+	"github.com/revel/revel"
+)
+
+var GITHUB = &oauth.Config{
+	ClientId:     "e484eb4a84a86e4f8267",
+	ClientSecret: "6b096615a2bfc91d9a8e8f0808216073d760f1fb",
+	AuthURL:      "https://github.com/login/oauth/authorize",
+	TokenURL:     "https://github.com/login/oauth/access_token",
+	RedirectURL:  "http://localhost:9000/App/Auth",
+}
 
 type App struct {
 	*revel.Controller
 }
 
 func (c App) Index() revel.Result {
-	return c.Render()
+	url := GITHUB.AuthCodeURL("state")
+	return c.Render(url)
+}
+
+func (c App) Auth(code string) revel.Result {
+	transport := &oauth.Transport{Config: GITHUB}
+	token, err := transport.Exchange(code)
+	if err != nil {
+		revel.ERROR.Println(err)
+		return c.Redirect(App.Index)
+	}
+
+	accessToken := token.AccessToken
+	transport = &oauth.Transport{
+		Token: &oauth.Token{AccessToken: accessToken},
+	}
+
+	client := github.NewClient(transport.Client())
+	repos, _, _ := client.Repositories.List("octocat", nil)
+	for _, repo := range repos {
+		fmt.Println(*repo.Name)
+	}
+	return c.Redirect(App.Index)
 }
